4712781449b55afbbfb2cbaabd2bc639b455de58
Wells <chawkeye@gmail.com>
2018-09-06 22:55:37 +0800

    訂單增加是否付款欄位

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180904135536) do
+ActiveRecord::Schema.define(version: 20180906145455) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20180904135536) do
     t.integer "total_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_paid", default: false
   end
 
   create_table "products", force: :cascade do |t|
